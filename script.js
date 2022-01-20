12c07e7dd7912dfa683a1c3723b1f1297467e9ce
Lorenzo Martins Rodrigues <82847299+lorenzonfts@users.noreply.github.com>
2022-01-20 15:07:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('btn-orange').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "orange");
+});
+
+document.getElementById('btn-red').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "red");
+});
+
+
+document.getElementById('btn-purple').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "purple");
+});
+
+
+document.getElementById('btn-black').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "black");
+});
+
+document.getElementById('btn-blue').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "blue");
+});
+
+document.getElementById('btn-grey').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "grey");
+});
+
+
+document.getElementById('btn-tomato').addEventListener('click', function(){
+  document.querySelector("body").setAttribute("class", "tomato");
+});
+
+
+
+
+
